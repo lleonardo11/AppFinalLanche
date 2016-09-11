ded1d03515670420c026d1df00dd4cedae361932
Leonardo Oliveira <leonardo_cxm@hotmail.com>
2016-09-11 02:19:21 -0300

    App de lanche
    Aplicativo

---
+++ LancheFinal/app/src/main/java/br/ufms/leonardo/lanches/data/ListaDeLanches.java
@@ -0,0 +1,36 @@
+package br.ufms.leonardo.cardapiovirtual.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufms.leonardo.cardapiovirtual.model.Lanche;
+
+public class ListaDeLanches {
+
+    private static ListaDeLanches instance;
+
+    private List<Lanche> lanches;
+
+    private ListaDeLanches() {
+        lanches = new ArrayList<Lanche>();
+    }
+
+    public static ListaDeLanches getInstance() {
+        if (instance == null) {
+            instance = new ListaDeLanches();
+        }
+        return instance;
+    }
+
+    public static void setInstance(ListaDeLanches instance) {
+        ListaDeLanches.instance = instance;
+    }
+
+    public List<Lanche> getLanches() {
+        return lanches;
+    }
+
+    public void setLanches(List<Lanche> lanches) {
+        this.lanches = lanches;
+    }
+}
